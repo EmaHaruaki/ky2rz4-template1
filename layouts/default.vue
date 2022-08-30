8a774cf5eb8bb1e8e94507da6b7ff7ea7ba55df8
EmaHaruaki <hase.yrsk@gmail.com>
2022-08-30 23:36:49 +0800

    update:srcset追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,7 +30,7 @@
 						<div class="site-branding">
 							<div class="site-logo">
 								<nuxt-link to="/" rel="home">
-									<img alt="デモサイト" src="https://ky2rz4.g.kuroco-img.app/files/user/header_img/AdobeStock_442660225.jpeg?width=230" width="230" />
+									<img alt="デモサイト" src="https://ky2rz4.g.kuroco-img.app/files/user/header_img/AdobeStock_442660225.png?width=800&height=160" width="250" />
 								</nuxt-link>
 							</div>
 							<h1 class="site-title"><nuxt-link to="/" rel="home">デモサイト</nuxt-link></h1>
