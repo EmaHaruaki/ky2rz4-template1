0aaa82f841e15608cffd628ed2885c4a16d72de1
EmaHaruaki <hase.yrsk@gmail.com>
2022-10-01 23:15:22 +0800

    Revert "update:採用情報追加"
    This reverts commit 2a2a43fa43d3602ac787431c5ac03ff894c564de.

--- layouts/default.vue
+++ layouts/default.vue
@@ -77,12 +77,12 @@
 											<nuxt-link to="/news">お知らせ</nuxt-link>
 										</li>
 										<li class="menu-item"
-											:class="{'current-menu-item current_page_item':$route.path === path_careers}">
-											<nuxt-link to="/careers">採用情報</nuxt-link>
-										</li>
-										<li class="menu-item"
 											:class="{'current-menu-item current_page_item':$route.path === path_contact}">
 											<nuxt-link to="/contact">お問い合わせ</nuxt-link>
+										</li>
+										<li class="menu-item"
+											:class="{'current-menu-item current_page_item':$route.path === path_blog}">
+											<nuxt-link to="/blog">ブログ</nuxt-link>
 										</li>
 									</ul>
 								</div>
@@ -150,13 +150,13 @@
 											</li>
 											<li @click="closemenu"
 												class="menu-item"
-												:class="{'current-menu-item current_page_item':$route.path === path_careers}">
-												<nuxt-link to="/careers">採用情報</nuxt-link>
+												:class="{'current-menu-item current_page_item':$route.path === path_contact}">
+												<nuxt-link to="/contact">お問い合わせ</nuxt-link>
 											</li>
 											<li @click="closemenu"
 												class="menu-item"
-												:class="{'current-menu-item current_page_item':$route.path === path_contact}">
-												<nuxt-link to="/contact">お問い合わせ</nuxt-link>
+												:class="{'current-menu-item current_page_item':$route.path === path_blog}">
+												<nuxt-link to="/blog">ブログ</nuxt-link>
 											</li>
 										</ul>
 									</div>
@@ -218,10 +218,6 @@
 											<nuxt-link to="/news">お知らせ</nuxt-link>
 										</li>
 										<li class="menu-item"
-											:class="{'current-menu-item current_page_item':$route.path === path_careers}">
-											<nuxt-link to="/careers">採用情報</nuxt-link>
-										</li>
-										<li class="menu-item"
 											:class="{'current-menu-item current_page_item':$route.path === path_contact}">
 											<nuxt-link to="/contact">お問い合わせ</nuxt-link>
 										</li>
@@ -272,10 +268,6 @@
 									<li class="menu-item"
 										:class="{'current-menu-item current_page_item':$route.path === path_news}">
 										<nuxt-link to="/news">お知らせ</nuxt-link>
-									</li>
-									<li class="menu-item"
-										:class="{'current-menu-item current_page_item':$route.path === path_careers}">
-										<nuxt-link to="/careers">採用情報</nuxt-link>
 									</li>
 									<li class="menu-item"
 										:class="{'current-menu-item current_page_item':$route.path === path_contact}">
@@ -333,7 +325,6 @@ export default {
 			path_service: "/service",
 			path_aboutus: "/about_us",
 			path_news: "/news",
-			path_careers: "/careers",
 			response_homeheader: null
 		}
 	},
